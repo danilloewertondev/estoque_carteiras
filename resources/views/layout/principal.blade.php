2532f5fb077739bec41ce12f872523a96a25aa1d
danillotorres <danillo.torres@outlook.com>
2017-05-24 12:00:40 -0300

    inserindo view com menu

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -2,11 +2,17 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="/css/app.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sistema de Estoque</title>
 </head>
 <body>
 
-@yield('conteudo')
+	@yield('conteudo')
+
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
 
 </body>
 </html>
--- resources/views/produtos/listagem.blade.php
+++ resources/views/produtos/listagem.blade.php
@@ -1,30 +1,35 @@
 @extends('layout.principal')
+@extends('layout.menu')
 
-@section('conteudo')	
-<table class="table">
-	@foreach ($produtos as $p)
-	<tr> 
-		<td>{{$p->cor}}</td>
-		<td>{{$p->quantidade}}</td>
-		<td>{{$p->referencia->nome_referencia}} </td>
-		<td>{{$p->tipodecouro->nome_couro}} </td>
+@section('conteudo')
+@section('menu')
+<div class="container">
+	<table class="table">
+		@foreach ($produtos as $p)
+		<tr> 
+			<td>{{$p->cor}}</td>
+			<td>{{$p->quantidade}}</td>
+			<td>{{$p->referencia->nome_referencia}} </td>
+			<td>{{$p->tipodecouro->nome_couro}} </td>
 
-		<td> 
-			<a href="{{action('ProdutoController@remove', $p->id)}}"> 
-				<span class="glyphicon glyphicon-trash"></span>
-			</a>
-		</td>
+			<td> 
+				<a href="{{action('ProdutoController@remove', $p->id)}}"> 
+					<span class="glyphicon glyphicon-trash"></span>
+				</a>
+			</td>
 
-		<td> 
-			<a href="/produtos/busca/{{$p->id}}"> 
-				<span class="glyphicon glyphicon-pencil"></span>
-			</a>
-		</td>
+			<td> 
+				<a href="/produtos/busca/{{$p->id}}"> 
+					<span class="glyphicon glyphicon-pencil"></span>
+				</a>
+			</td>
 
 
 
-	</tr>
-	@endforeach
-</table>
+		</tr>
+		@endforeach
+	</table>
+</div>
+@stop
 @stop
 
