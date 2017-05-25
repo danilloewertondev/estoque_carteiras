2c629708000b136f8c42e7e4c937b219ec42fddd
danillotorres <danillo.torres@outlook.com>
2017-05-25 11:03:25 -0300

    adicionando front end da pagina de login

--- resources/views/form_login.blade.php
+++ resources/views/form_login.blade.php
@@ -12,24 +12,37 @@
 </div>
 @endif
 
-<h1>Login</h1>
 
-<form action="/login" method="post">
+<header>
+	
 
-	<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 
-	<div class="form-group">
-		<label>E-mail</label>
-		<input name="email" class="form-control" />
+
+	<div class="carteira-banner">
+	<div ><h2 id="tituloCenter">Sistema de Estoque de Carteiras</h2></div><br>
+
+			<form class="center" action="/login" method="post">
+
+				<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+
+				<div class="form-group col-sm-5 col-md-5">
+					<label>E-mail</label>
+					<input name="email" class="form-control" />
+				</div>
+
+				<div class="form-group col-sm-3 col-md-4">
+					<label>Senha</label>
+					<input type="password" name="password" class="form-control" />
+				</div>	
+
+				<div class="form-group col-sm-3 col-md-2 margem">
+					<button type="submit" class="btn 
+					btn-primary btn-block">Login</button>
+				</div>
+			</form>
+		
 	</div>
 
-	<div class="form-group">
-		<label>Senha</label>
-		<input type="password" name="password" class="form-control" />
-	</div>	
-	
-	<button type="submit" class="btn 
-	btn-primary btn-block">Login</button>
-</form>
+</header>
 
 @stop
